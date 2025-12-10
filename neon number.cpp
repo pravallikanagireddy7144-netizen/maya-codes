7264f19ac668b60a7e4b65d803c5698dad7b9ed0
pravallikanagireddy7144-netizen <pravallikanagireddy7144@gmail.com>
2025-12-10 19:32:39 +0530

    Add files via upload

---
+++ neon number.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int n,r,t,s;
+    scanf("%d",&n);
+    s=n*n;
+    while (s!=0)
+    {
+      r=s%10;
+      s=s/10;
+      t=t+r; 
+    }
+    if (t==n)
+    {printf("Neon Number");}
+    else
+    {printf("Not Neon Number");}
+}
