4c48c741bfc1fea750fd311f5717b32b1229aa5d
pravallikanagireddy7144-netizen <pravallikanagireddy7144@gmail.com>
2025-12-10 19:55:15 +0530

    Add files via upload

---
+++ character is uppercase or lowercase.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if (a>='A' && a<='Z')
+    {
+        printf("uppercase alphabet");
+    }
+    else if (a>='a' && a<='z')
+    {
+        printf("lowercase alphabet");
+    }
+    else
+    {
+        printf("not an alphabet");
+    }
+}
